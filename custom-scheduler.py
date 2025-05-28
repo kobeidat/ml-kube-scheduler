0bc75629860eb443e93aa0efb63faf81a598d985
Kamil Obeidat <kobeidat@student.agh.edu.pl>
2025-05-28 11:09:04 +0200

    Add the actual scheduler (wrong machine)

--- custom-scheduler.py
+++ custom-scheduler.py
@@ -4,13 +4,27 @@ import random
 import json
 from time import localtime, strftime
 from kubernetes import client, config, watch
+import requests
 
 timestamp_format = "%Y-%m-%d %H:%M:%S"
-scheduler_name = "custom-scheduler"
+scheduler_name = "my-scheduler"
+prometheus_url = "http://prometheus-server.default.svc.cluster.local:9090"
 
 # Use load_incluster_config when deploying scheduler from within the cluster. Otherwise use load_kube_config
 config.load_incluster_config()
 v1 = client.CoreV1Api()
+
+def get_timestamp():
+  return strftime(timestamp_format, localtime())
+
+def query_prometheus(query):
+  try:
+    r = requests.get(f"{prometheus_url}/api/v1/query", params={'query': query})
+    print(f"Resp: {r.json()}")
+    return r.json().get("data", {}).get("result", [])
+  except Exception as e:
+    print(f"Custom-Scheduler: Error querying Prometheus: {e}")
+    return []
 
 def get_timestamp():
     return strftime(timestamp_format, localtime())
@@ -46,8 +60,16 @@ def main():
         # and event['object'].spec.scheduler_name == scheduler_name:
         if event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == scheduler_name:
             try:
+                print(event['object'].metadata.__dict__)
                 pod_name = event['object'].metadata.name
+                print("...")
+                prometheus_data = query_prometheus('100 - (avg(irate(node_cpu_seconds_total{mode="idle"}[30m])) * 100)')
+                print("...")
+                print(f"Custom-Scheduler: Prometheus data: {prometheus_data}")
+                print("...")
                 nodes_list = list(set(nodes_available()))
+                print("...")
+                print(f"Custom-Scheduler: Nodes: {nodes_list}")
                 random_node = random.choice(nodes_list)
                 res = scheduler(pod_name, random_node)
                 print("Custom-Scheduler: {}: Scheduling result: {}".format(get_timestamp(), res.status))
