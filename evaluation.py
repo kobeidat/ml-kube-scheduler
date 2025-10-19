20ab7d5c34c154f8d06fef9ccd3d09a36f8dcdb4
mpus <mateuszpodmokly@outlook.com>
2025-10-20 00:53:26 +0200

    evaluation logging basics

--- evaluation.py
+++ evaluation.py
@@ -1,7 +1,16 @@
 from kubernetes import config, client, utils
-from prometheus import query_prometheus_cpu
+from prometheus import query_prometheus_cpu, get_timestamp
 import numpy as np
 from time import sleep
+import json
+import pandas as pd
+
+# ================================================== #
+# do zrobienia:
+# metryki do ewaluacji (np wariancja RAM i inne)
+# automatyzacja ewaluacji
+# zapisywanie wynikow do pliku
+# ================================================== #
 
 
 def cpu_variance():
@@ -13,25 +22,32 @@ def cpu_variance():
 
     cpu_usage = np.array(cpu_usage) * 100
     var = np.var(cpu_usage)
-    print("CPU usage: " + np.round(cpu_usage, 4))
-    print("CPU variance: "+ np.round(var, 4))
+    return var
 
 def evaluate():
+    config.load_kube_config()
+    client = client.ApiClient()
+
+    var1 = cpu_variance()
     print("Before deployment:")
-    cpu_variance()
+    print("CPU variance: " + np.round(var1, 4))
+
     utils.create_from_yaml(client, "pods/cpu-stress-deploy.yaml")
+    var2 = cpu_variance()
     print("After deployment:")
-    cpu_variance()
+    print("CPU variance: " + np.round(var2, 4))
 
     sleep(60)
+    var3 = cpu_variance()
     print("After 60 seconds:")
-    cpu_variance()
+    print("CPU variance: " + np.round(var3, 4))
+
+    file = open("logs/eval.json", "a", encoding="utf-8")
+    data = {"timestamp": get_timestamp(), "var1": var1, "var2": var2, "var3": var3}
+    file.write(json.dumps(data, ensure_ascii=False) + "\n")
+    file.close()
 
 
-config.load_kube_config()
-client = client.ApiClient()
-
-# do zrobienia:
-# metryki do ewaluacji (np wariancja RAM i inne)
-# automatyzacja ewaluacji
-# zapisywanie wynikow do pliku
+if __name__ == "__main__":
+    df = pd.read_json("logs/eval.json", lines=True)
+    print(df)
